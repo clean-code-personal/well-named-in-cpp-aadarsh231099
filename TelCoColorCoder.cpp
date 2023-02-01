f7f19da5a5513df1c4932ac13506cc1adfa20171
Aadarsh <aadarshhanu23@gmail.com>
2023-02-01 14:34:33 +0530

    commit to move tostring function to header file

--- TelCoColorCoder.cpp
+++ TelCoColorCoder.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
 #include <assert.h>
-#include "TelCoColorCoder.h"
+#include <TelCoColorCoder.h>
 
-std::string TelCoColorCoder::ColorPair::ToString() {
-    std::string colorPairStr = MajorColorNames[majorColor];
-    colorPairStr += " ";
-    colorPairStr += MinorColorNames[minorColor];
-    return colorPairStr;
-}
+using namespace TelCoColorCoder;
 
-TelCoColorCoder::ColorPair TelCoColorCoder::GetColorFromPairNumber(int pairNumber) {
+ColorPair GetColorFromPairNumber(int pairNumber) {
     int zeroBasedPairNumber = pairNumber - 1;
-    MajorColor majorColor =
+    MajorColor majorColor = 
         (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
     MinorColor minorColor =
         (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
     return ColorPair(majorColor, minorColor);
 }
 
-int TelCoColorCoder::GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
     return major * numberOfMinorColors + minor + 1;
 }
